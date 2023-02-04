05f15c15f10fe402ddac96a81f4ed605137dfb3a
Vladimir <dr.frisbi@gmail.com>
2023-02-05 00:02:29 +0700

    add code 047, 052

---
+++ 052/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void FillArrayMidl(int[,] matr)
+{
+    Console.Write("Среднее арифметическое элементов в каждом столбце: ");
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            sum += matr[i, j];
+        }
+        Console.Write($"{sum / matr.GetLength(0)}, ");
+    }
+}
+
+int[,] matrix = new int[4, 4];
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+FillArrayMidl(matrix);
